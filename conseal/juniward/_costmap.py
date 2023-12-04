689f1825805f451f49599f7a809f3269d4cc93c2
Benedikt Lorch <benedikt.lorch@uibk.ac.at>
2023-12-04 16:24:12 +0100

    Minor edits

--- conseal/juniward/_costmap.py
+++ conseal/juniward/_costmap.py
@@ -11,9 +11,7 @@ Affiliation: University of Innsbruck
 
 This implementation builds on the original Matlab implementation provided by the paper authors. Please find that license of the original implementation below.
 -------------------------------------------------------------------------
-Copyright (c) 2013 DDE Lab, Binghamton University, NY.
-All Rights Reserved.
--------------------------------------------------------------------------
+Copyright (c) 2013 DDE Lab, Binghamton University, NY. All Rights Reserved.
 Permission to use, copy, modify, and distribute this software for educational, research and non-profit purposes, without fee, and without a written agreement is hereby granted, provided that this copyright notice appears in all copies. The program is supplied "as is," without any accompanying services from DDE Lab. DDE Lab does not warrant the operation of the program will be uninterrupted or error-free. The end-user understands that the program was developed for research purposes and is advised not to rely exclusively on the program for any reason. In no event shall Binghamton University or DDE Lab be liable to any party for direct, indirect, special, incidental, or consequential damages, including lost profits, arising out of the use of this software. DDE Lab disclaims any warranties, and has no obligations to provide maintenance, support, updates, enhancements or modifications.
 -------------------------------------------------------------------------
 """
