5eddfa38f5ae893cbca6cb56a7912791014604a1
Benedikt Lorch <benedikt.lorch@uibk.ac.at>
2023-12-06 18:07:07 +0100

    Update J-UNIWARD tests

--- test/test_juniward.py
+++ test/test_juniward.py
@@ -13,37 +13,40 @@ COVER_DIR = ASSETS_DIR / 'cover'
 
 
 class TestJUNIWARD(unittest.TestCase):
-    """Test suite for J-UNIWARD embedding."""
+    '''Test suite for J-UNIWARD embedding.'''
     _logger = logging.getLogger(__name__)
 
     @staticmethod
-    def read_costmap(probability_map_filename, height, width):
-        """
+    def read_costmap(costmap_filename, height, width):
+        '''
         Read binary file produced by a modified J-UNIWARD C++ implementation
-        :param probability_map_filename: path to binary file
+        :param costmap_filename: path to binary file
         :param height: of the original image
         :param width: of the original image
         :return: ndarray of shape [height, width, 3]
             Channel 0: If the cover pixel is at the minimum -1023 already, the pixel contains the wet cost; otherwise rho.
             Channel 1: Always 0.
             Channel 2: If the cover pixel is at the maximum 1023 already, the pixel contains the wet cost; otherwise rho.
-        """
+        '''
         count = height * width * 3
-        with open(probability_map_filename, 'rb') as f:
-            probability_map = np.fromfile(f, dtype=np.float32, count=count, sep='')
-            probability_map = probability_map.reshape(height, width, 3)
-        return probability_map
+        with open(costmap_filename, 'rb') as f:
+            costmap = np.fromfile(f, dtype=np.float32, count=count, sep='')
+            costmap = costmap.reshape(height, width, 3)
+        return costmap
 
     @parameterized.expand([
-        ("lizard_gray.jpeg", "lizard_gray.costmap"),
-        ("mountain_gray.jpeg", "mountain_gray.costmap"),
-        ("nuclear_gray.jpeg", "nuclear_gray.costmap"),
+        # ('lizard_gray.jpeg', 'costmap-cpp-original/lizard_gray.costmap', cl.juniward.Implementation.JUNIWARD_ORIGINAL),
+        # ('mountain_gray.jpeg', 'costmap-cpp-original/mountain_gray.costmap', cl.juniward.Implementation.JUNIWARD_ORIGINAL),
+        # ('nuclear_gray.jpeg', 'costmap-cpp-original/nuclear_gray.costmap', cl.juniward.Implementation.JUNIWARD_ORIGINAL),
+        ('lizard_gray.jpeg', 'lizard_gray_fix.costmap', cl.juniward.Implementation.JUNIWARD_FIX_OFF_BY_ONE),
+        ('mountain_gray.jpeg', 'mountain_gray_fix.costmap', cl.juniward.Implementation.JUNIWARD_FIX_OFF_BY_ONE),
+        ('nuclear_gray.jpeg', 'nuclear_gray_fix.costmap', cl.juniward.Implementation.JUNIWARD_FIX_OFF_BY_ONE),
     ])
-    def test_costmap_original_cpp_python_equivalence(self, cover_filename, costmap_cpp_filename):
-        self._logger.info(f'TestJUNIWARD.test_probability_map_original_cpp_python_equivalence({cover_filename=}, {costmap_cpp_filename=})')
+    def test_costmap_cpp_python_equivalence(self, cover_filename, costmap_cpp_filename, implementation):
+        self._logger.info(f'TestJUNIWARD.test_costmap_fix_cpp_python_equivalence({cover_filename=}, {costmap_cpp_filename=}, {implementation=})')
 
         cover_filepath = COVER_DIR / cover_filename
-        costmap_cpp_filepath = STEGO_DIR / 'costmap-cpp-original' / costmap_cpp_filename
+        costmap_cpp_filepath = STEGO_DIR / 'costmap-cpp' / costmap_cpp_filename
 
         cover_spatial = np.squeeze(jpeglib.read_spatial(cover_filepath).spatial[..., 0]).astype(np.float64)
         img_dct = jpeglib.read_dct(cover_filepath)
@@ -51,13 +54,14 @@ class TestJUNIWARD(unittest.TestCase):
         quantization_table = img_dct.qt[0]
 
         # Compute cost for embedding into the quantized DCT coefficients of shape [num_vertical_blocks, num_horizontal_blocks, 8, 8]
+        wet_cost = 10 ** 13
         rho_p1, rho_m1 = cl.juniward.compute_cost_adjusted(
             cover_spatial=cover_spatial,
             cover_dct_coeffs=cover_dct_coeffs,
             quantization_table=quantization_table,
             dtype=np.float64,
-            implementation=cl.juniward.JUNIWARD_FIX_OFF_BY_ONE,
-            wet_cost=10**13,
+            implementation=implementation,
+            wet_cost=wet_cost,
         )
 
         # Rearrange from 4D to 2D
@@ -65,16 +69,21 @@ class TestJUNIWARD(unittest.TestCase):
         rho_m1_2d = cl.tools.dct.jpeglib_to_jpegio(rho_m1)
 
         costmap_cpp = self.read_costmap(costmap_cpp_filepath, 512, 512)
-        np.testing.assert_allclose(rho_m1_2d, costmap_cpp[:, :, 0])
-        np.testing.assert_allclose(rho_p1_2d, costmap_cpp[:, :, 2])
+
+        # Set rtol and atol to the default values used by np.allclose()
+        np.testing.assert_allclose(rho_m1_2d, costmap_cpp[:, :, 0], rtol=1e-05, atol=1e-08)
+        np.testing.assert_allclose(rho_p1_2d, costmap_cpp[:, :, 2], rtol=1e-05, atol=1e-08)
 
     @parameterized.expand([
-        ("lizard_gray.jpeg", "lizard_gray_costmap.mat"),
-        ("mountain_gray.jpeg", "mountain_gray_costmap.mat"),
-        ("nuclear_gray.jpeg", "nuclear_gray_costmap.mat"),
+        ('lizard_gray.jpeg', 'lizard_gray_costmap_original.mat', cl.JUNIWARD_ORIGINAL),
+        ('mountain_gray.jpeg', 'mountain_gray_costmap_original.mat', cl.JUNIWARD_ORIGINAL),
+        ('nuclear_gray.jpeg', 'nuclear_gray_costmap_original.mat', cl.JUNIWARD_ORIGINAL),
+        ('lizard_gray.jpeg', 'lizard_gray_costmap_fix.mat', cl.JUNIWARD_FIX_OFF_BY_ONE),
+        ('mountain_gray.jpeg', 'mountain_gray_costmap_fix.mat', cl.JUNIWARD_FIX_OFF_BY_ONE),
+        ('nuclear_gray.jpeg', 'nuclear_gray_costmap_fix.mat', cl.JUNIWARD_FIX_OFF_BY_ONE),
     ])
-    def test_costmap_original_matlab_python_equivalence(self, cover_filename, costmap_matlab_filename):
-        self._logger.info(f'TestJUNIWARD.test_costmap_original_matlab_python_equivalence({cover_filename=}, {costmap_matlab_filename=})')
+    def test_costmap_matlab_python_equivalence(self, cover_filename, costmap_matlab_filename, implementation):
+        self._logger.info(f'TestJUNIWARD.test_costmap_matlab_python_equivalence({cover_filename=}, {costmap_matlab_filename=}, {implementation=})')
 
         cover_filepath = COVER_DIR / cover_filename
         costmap_matlab_filepath = STEGO_DIR / 'costmap-matlab' / costmap_matlab_filename
@@ -86,7 +95,7 @@ class TestJUNIWARD(unittest.TestCase):
         costmap = cl.juniward._costmap.compute_cost(
             spatial=img_spatial,
             quantization_table=qt,
-            implementation=cl.JUNIWARD_ORIGINAL,
+            implementation=implementation,
         )
 
         # Convert from 4D to 2D
@@ -97,15 +106,15 @@ class TestJUNIWARD(unittest.TestCase):
         np.testing.assert_allclose(costmap, costmap_matlab)
 
     @parameterized.expand([
-        ("lizard_gray.jpeg", "lizard_gray_matlab_alpha_0.4_seed_6020.jpeg", 0.4, 6020),
-        ("mountain_gray.jpeg", "mountain_gray_matlab_alpha_0.4_seed_6020.jpeg", 0.4, 6020),
-        ("nuclear_gray.jpeg", "nuclear_gray_matlab_alpha_0.4_seed_6020.jpeg", 0.4, 6020),
+        ('lizard_gray.jpeg', 'lizard_gray_matlab_alpha_0.4_seed_6020.jpeg', 0.4, 6020),
+        ('mountain_gray.jpeg', 'mountain_gray_matlab_alpha_0.4_seed_6020.jpeg', 0.4, 6020),
+        ('nuclear_gray.jpeg', 'nuclear_gray_matlab_alpha_0.4_seed_6020.jpeg', 0.4, 6020),
     ])
     def test_simulation_python_matlab_equivalence(self, cover_filename, stego_filename, embedding_rate, seed):
         self._logger.info(f'TestJUNIWARD.test_simulation_python_matlab_equivalence('f'{cover_filename=}, {embedding_rate=}, {seed=})')
 
         cover_filepath = COVER_DIR / cover_filename
-        stego_matlab_filepath = STEGO_DIR / "stego-matlab" / stego_filename
+        stego_matlab_filepath = STEGO_DIR / 'stego-matlab' / stego_filename
 
         # Read grayscale image
         cover_spatial = jpeglib.read_spatial(cover_filepath).spatial[:, :, 0]
@@ -134,4 +143,4 @@ class TestJUNIWARD(unittest.TestCase):
         np.testing.assert_allclose(stego_dct_coeffs, stego_matlab_dct_coeffs)
 
 
-__all__ = ["TestJUNIWARD"]
+__all__ = ['TestJUNIWARD']
