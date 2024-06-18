50e65016f18c18d6955c897ab1a9f2e24db9fc7b
Martin Benes <martin.benes@uibk.ac.at>
2024-06-18 15:24:16 +0200

    disabled ebs tests

--- test/test_ebs.py
+++ test/test_ebs.py
@@ -43,28 +43,28 @@ class TestEBS(unittest.TestCase):
         rho_remi = remi[f'rhos']
         self.assertTrue(np.allclose(rho, rho_remi))
 
-    # @parameterized.expand([[f] for f in defs.TEST_IMAGES])
-    @parameterized.expand([['seal1']])
-    def test_simulate(self, fname):
-        self._logger.info(f'TestEBS.test_simulate({fname})')
-        # load cover
-        cover = jpeglib.read_dct(defs.COVER_DIR / 'jpeg_75_gray' / f'{fname}.jpg')
+    # # @parameterized.expand([[f] for f in defs.TEST_IMAGES])
+    # @parameterized.expand([['seal1']])
+    # def test_simulate(self, fname):
+    #     self._logger.info(f'TestEBS.test_simulate({fname})')
+    #     # load cover
+    #     cover = jpeglib.read_dct(defs.COVER_DIR / 'jpeg_75_gray' / f'{fname}.jpg')
 
-        # simulate
-        stego = cl.ebs.simulate_single_channel(
-            cover.Y,
-            cover.qt[0],
-            .4,
-            implementation=cl.EBS_ORIGINAL,
-            seed=12345,
-        )
+    #     # simulate
+    #     stego = cl.ebs.simulate_single_channel(
+    #         cover.Y,
+    #         cover.qt[0],
+    #         .4,
+    #         implementation=cl.EBS_ORIGINAL,
+    #         seed=12345,
+    #     )
 
-        # load reference stego
-        stego_ref = jpeglib.read_dct(defs.ASSETS_DIR / 'stego_matlab' / 'ebs' / f'{fname}.jpg').Y
+    #     # load reference stego
+    #     stego_ref = jpeglib.read_dct(defs.ASSETS_DIR / 'stego_matlab' / 'ebs' / f'{fname}.jpg').Y
 
-        # compare stegos
-        # np.testing.assert_array_equal(stego, stego_ref)
-        # EBS simulator contains bugs
+    #     # compare stegos
+    #     # np.testing.assert_array_equal(stego, stego_ref)
+    #     # EBS simulator contains bugs
 
 
 __all__ = ["TestEBS"]
